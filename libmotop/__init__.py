f6a58d78aeec99474154762f11c4d3de1769e218
Paul Ridgway <paul@thefloow.com>
2017-07-05 08:53:42 +0100

    Update notice

--- libmotop/__init__.py
+++ libmotop/__init__.py
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2012, Tart İnternet Teknolojileri Ticaret AŞ
 #
+# Modified 2017, Paul Ridgway
+#
 # Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
 # granted, provided that the above copyright notice and this permission notice appear in all copies.
 #
